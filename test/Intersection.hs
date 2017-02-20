82451f354dbe2dd09f644750e86edd202a84d1f4
Robin Raymond <robin@robinraymond.de>
2017-02-20 21:17:16 +0100

    adjust tests to new interface

--- test/Intersection.hs
+++ test/Intersection.hs
@@ -14,17 +14,17 @@ main :: IO ()
 main = hspec $ do
     describe "Intersecting spheres and rays" $ do
         it "sphere ray double intersection" $ do
-            let s = OTY.Sphere (LAF.P $ LV3.V3 0 0 0) 1
-                r = OTY.newRay (LAF.P $ LV3.V3 2 0 0) (LAF.P $ LV3.V3 (-1) 0 0)
-            (OIN.intersectSphereRay s r) `shouldBe` (Just $ LAF.P $ LV3.V3 1 0 0)
+            let s = OTY.Sphere (OTY.mkP 0 0 0) 1
+                r = OTY.mkRay (OTY.mkP 2 0 0) (OTY.mkP (-1) 0 0)
+            (OIN.intersectSphereRay s r) `shouldBe` (Just $ OTY.mkP 1 0 0)
 
         it "sphere ray no intersection" $ do
-            let s = OTY.Sphere (LAF.P $ LV3.V3 0 0 0) 1
-                r = OTY.newRay (LAF.P $ LV3.V3 2 0 0) (LAF.P $ LV3.V3 3 0 0)
+            let s = OTY.Sphere (OTY.mkP 0 0 0) 1
+                r = OTY.mkRay (OTY.mkP 2 0 0) (OTY.mkP 3 0 0)
             (OIN.intersectSphereRay s r) `shouldBe` Nothing
 
         it "sphere ray single intersection" $ do
-            let s = OTY.Sphere (LAF.P $ LV3.V3 0 0 0) 1
-                r = OTY.newRay (LAF.P $ LV3.V3 2 1 0) (LAF.P $ LV3.V3 0 1 0)
-            (OIN.intersectSphereRay s r) `shouldBe` (Just $ LAF.P $ LV3.V3 0 1 0)
+            let s = OTY.Sphere (OTY.mkP 0 0 0) 1
+                r = OTY.mkRay (OTY.mkP 2 1 0) (OTY.mkP 0 1 0)
+            (OIN.intersectSphereRay s r) `shouldBe` (Just $ OTY.mkP 0 1 0)
 
