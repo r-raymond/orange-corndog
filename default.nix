981ca821b2bf815aeb629dad8cb2e0202fd89e50
Robin Raymond <robin@robinraymond.de>
2017-02-21 18:48:24 +0100

    start implementation of SDL renderer

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { mkDerivation, base, criterion, fast-logger, gitrev, hspec, linear
-, protolude, stdenv
+, protolude, SDL2, sdl2, stdenv
 }:
 mkDerivation {
   pname = "orange-corndog";
@@ -8,13 +8,14 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base criterion fast-logger gitrev hspec linear protolude
+    base criterion fast-logger gitrev hspec linear protolude sdl2
   ];
   executableHaskellDepends = [
-    base criterion fast-logger gitrev hspec linear protolude
+    base criterion fast-logger gitrev hspec linear protolude sdl2
   ];
+  executableSystemDepends = [ SDL2 ];
   testHaskellDepends = [
-    base criterion fast-logger gitrev hspec linear protolude
+    base criterion fast-logger gitrev hspec linear protolude sdl2
   ];
   homepage = "https://github.com/r-raymond/orange-corndog#readme";
   description = "A quick pathtracer";
--- src/OrangeCorndog/Main.hs
+++ src/OrangeCorndog/Main.hs
@@ -4,6 +4,28 @@ module OrangeCorndog.Main
 
 import Protolude
 
+import qualified SDL
+
 main :: IO ()
 main = do
-    putStrLn ("Hello World" :: Text)
+    SDL.initialize [SDL.InitVideo, SDL.InitEvents]
+
+    v <- SDL.version
+    print v
+
+    w <- SDL.createWindow "Yolo" SDL.defaultWindow
+
+    r <- SDL.createRenderer w (-1) SDL.defaultRenderer
+
+    loop r
+
+    SDL.quit
+
+loop :: SDL.Renderer -> IO ()
+loop r = do
+    t <- SDL.createTexture r SDL.RGBA8888 SDL.TextureAccessStreaming (SDL.V2 640 480)
+    t2 <- SDL.updateTexture t (Just (SDL.Rectangle (SDL.P (SDL.V2 0 0)) (SDL.V2 2 2)))
+                            "abcdefghiklmandr" 8
+    SDL.clear r
+    SDL.copy r t2 Nothing Nothing
+    SDL.present r
